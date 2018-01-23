a0ae244b185a20d85e92468117120d7945fcaef9
Szymon Lankiewicz <szymon.lankiewicz@gmail.com>
2018-01-24 00:12:01 +0100

    Hystrix - example 3

--- lending-service/src/main/java/com/gft/academy/selen/LendingServiceApplication.java
+++ lending-service/src/main/java/com/gft/academy/selen/LendingServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class LendingServiceApplication {
 
     public static void main(String[] args) {
