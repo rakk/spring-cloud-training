7bf1233fb110658eb8b6cabdcdcbdfa3e1c52941
rakk <Robert.Kapala@gmail.com>
2018-01-27 00:37:24 +0100

    Zipkin started receives requests. But still not matcher request across instances

--- tracing-service/src/main/java/com/cloud/training/tracingservice/TracingServiceApplication.java
+++ tracing-service/src/main/java/com/cloud/training/tracingservice/TracingServiceApplication.java
@@ -3,7 +3,9 @@ package com.cloud.training.tracingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TracingServiceApplication {
