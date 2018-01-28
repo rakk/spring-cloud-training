5b4bca9b11b452906851086e94e66c34129d61fe
przemyslaw.juszkiewicz@gmail.com <przemyslaw.juszkiewicz@gmail.com>
2018-01-28 09:37:22 +0100

    fix lending-ui add custom mapping for zipkin

--- lending-ui/src/main/java/com/gft/academy/selen/ui/LendingUIApplication.java
+++ lending-ui/src/main/java/com/gft/academy/selen/ui/LendingUIApplication.java
@@ -1,13 +1,9 @@
 package com.gft.academy.selen.ui;
 
-import com.gft.academy.selen.ui.feign.OAuthFeignRequestInterceptor;
-import feign.RequestInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableDiscoveryClient
@@ -15,11 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 @SpringBootApplication
 public class LendingUIApplication {
-
-    @Bean
-    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2ClientContext oauth2ClientContext) {
-        return new OAuthFeignRequestInterceptor(oauth2ClientContext);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(LendingUIApplication.class, args);
