efaa7f5dbe6cc00b56088f46a399e467ccae88eb
przemyslaw.juszkiewicz@gmail.com <przemyslaw.juszkiewicz@gmail.com>
2018-01-30 22:51:47 +0100

    prepare live-coding-routing branch initial state

--- routing-service/src/main/java/com/gft/academy/routing/RoutingServiceApplication.java
+++ routing-service/src/main/java/com/gft/academy/routing/RoutingServiceApplication.java
@@ -1,40 +1,13 @@
 package com.gft.academy.routing;
 
-import com.gft.academy.routing.fallback.DefaultFallback;
-import com.gft.academy.routing.fallback.WebFallback;
-import com.gft.academy.routing.filter.PreLogFilter;
-import com.gft.academy.routing.filter.VersionRouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
-@EnableZuulProxy
 @SpringBootApplication
 public class RoutingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoutingServiceApplication.class, args);
-    }
-
-    @Bean
-    DefaultFallback defaultFallback() {
-        return new DefaultFallback();
-    }
-
-    @Bean
-    WebFallback webFallback() {
-        return new WebFallback();
-    }
-
-    @Bean
-    PreLogFilter preLoginFilter() {
-        return new PreLogFilter();
-    }
-
-    @Bean
-    VersionRouteFilter versionRouteFilter() {
-        return new VersionRouteFilter();
     }
 
 }
