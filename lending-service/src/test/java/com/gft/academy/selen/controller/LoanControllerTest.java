c7bd023c2bf54f83f262beab77376565b7eb621e
rakk <Robert.Kapala@gmail.com>
2018-02-13 22:00:26 +0100

    Cleaning move tests

--- lending-service/src/test/java/com/gft/academy/selen/controller/LoanControllerTest.java
+++ lending-service/src/test/java/com/gft/academy/selen/controller/LoanControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = LoanController.class)
 public class LoanControllerTest {
